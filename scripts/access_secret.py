394d9c5e4235ff9c7a9df188d3a8b9b7f9e732ea
Brian Brown <brianbrown0417@gmail.com>
2024-05-15 18:48:58 -0400

    working get secret script, k8s access terraform

--- scripts/access_secret.py
+++ scripts/access_secret.py
@@ -1,14 +1,19 @@
 from google.cloud import secretmanager
 
-# Initialize the Secret Manager client
-client = secretmanager.SecretManagerServiceClient()
+def access_secret(project_id, secret_id, version):
+    """
+    Access a secret- API token, etc- stored in Secret Manager
+    """
+    client = secretmanager.SecretManagerServiceClient()
 
-# Specify the name of your secret
-secret_name = "projects/824544385686/secrets/google_maps_api_key"
+    # Build the resource name of the secret version
+    name = client.secret_version_path(
+        project=project_id, 
+        secret=secret_id, 
+        secret_version=version
+    )
 
-# Access the secret
-response = client.access_secret_version(request={"name": secret_name})
-secret_value = response.payload.data.decode("UTF-8")
+    # Access the secret version
+    response = client.access_secret_version(name=name).payload.data.decode("utf-8")
 
-# Use the secret value in your application
-print("Secret Value:", secret_value)
+    return response
